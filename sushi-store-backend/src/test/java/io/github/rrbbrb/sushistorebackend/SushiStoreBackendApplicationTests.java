b431e13ce6fc69462edfe8a3ca45f32c79cad015
Ruby Lai <rubellitel@gmail.com>
2020-06-12 16:44:30 +0800

    Adding back files

---
+++ sushi-store-backend/src/test/java/io/github/rrbbrb/sushistorebackend/SushiStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rrbbrb.sushistorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
