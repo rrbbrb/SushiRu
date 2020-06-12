57498f027af1dcdd33451f669f163c07660f6dc7
Ruby Lai <rubellitel@gmail.com>
2020-06-12 16:12:01 +0800

    Added SpringBoot starter

---
+++ sushi-store-backend/src/main/java/io/github/rrbbrb/sushistorebackend/SushiStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.rrbbrb.sushistorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiStoreBackendApplication.class, args);
+	}
+
+}
