cd89a9e69911b8e1d77a6fc750a2d7f2a6b7fd6d
Ruby Lai <rubellitel@gmail.com>
2020-06-25 17:04:50 +0800

    Added Angular components: authentication, login, signup, shopping-cart; deleted banner component

--- sushiru-client/client-website/src/app/app-routing.module.ts
+++ sushiru-client/client-website/src/app/app-routing.module.ts
@@ -2,11 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
 import { MenuItemsComponent } from './components/menu/menu-items/menu-items.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { SignupComponent } from './components/authentication/signup/signup.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './components/shopping-cart/checkout/checkout.component';
 
 const appRoutes: Routes = [
     { path: 'menu', component: MenuComponent, children: [
         { path: 'category', redirectTo: '/menu/category/1', pathMatch: 'full' },
         { path: 'category/:id', component: MenuItemsComponent }
+    ] },
+    { path: 'auth', component: AuthenticationComponent, children: [
+        { path: 'user', redirectTo: '/auth/user/login', pathMatch: 'full' },
+        { path: 'user/login', component: LoginComponent },
+        { path: 'user/signup', component: SignupComponent }
+    ] },
+    { path: 'cart', component: ShoppingCartComponent, children: [
+        { path: 'checkout', component: CheckoutComponent }
     ] },
     { path: '', redirectTo: 'menu/category', pathMatch: 'full'},
     { path: '**', redirectTo: 'menu/category', pathMatch: 'full'}
--- sushiru-client/client-website/src/app/app.module.ts
+++ sushiru-client/client-website/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { ProductService } from './services/product.service';
-import { BannerComponent } from './components/banner/banner.component';
 import { MenuItemsComponent } from './components/menu/menu-items/menu-items.component';
 
 import {CardModule} from 'primeng/card';
 import {InputNumberModule} from 'primeng/inputnumber';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { SignupComponent } from './components/authentication/signup/signup.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './components/shopping-cart/checkout/checkout.component';
 
 
 @NgModule({
@@ -20,8 +24,12 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     HeaderComponent,
     MenuComponent,
-    BannerComponent,
-    MenuItemsComponent
+    MenuItemsComponent,
+    AuthenticationComponent,
+    LoginComponent,
+    SignupComponent,
+    ShoppingCartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
